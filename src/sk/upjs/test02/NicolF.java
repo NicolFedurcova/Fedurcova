840c28c207705a87fcf350fc324c32cb5724c90d
24sas <24sas@158.197.236.56>
2022-09-28 18:17:04 +0200

    vymazala som komenty

--- src/sk/upjs/test02/NicolF.java
+++ src/sk/upjs/test02/NicolF.java
@@ -4,8 +4,10 @@ public class NicolF {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("ahoj nicol");
-		System.out.println("ahoj nicol tu sasa ");
+		System.out.println("ahoj to som ja sasa");
+		
+		
+		
 		
 		
 		
